61788161dd61ab082aac883e9d1ad82a05f336f6
Glitch (hyper-gusty-node) <none>
2021-07-12 15:32:16 +0000

    👦🎨 Checkpoint ./komutlar/guildInfo.js:31988211/626

--- komutlar/guildInfo.js
+++ komutlar/guildInfo.js
@@ -3,13 +3,37 @@ const ayarlar = require('../ayarlar.json')
 
 exports.run = async (client, message, args) => {
 let guild = message.guild.id;   
+  
 var verfyseviye = client.guilds.cache.get(guild).verificationLevel;
- var mfalev = nico.guilds.cache.get(guild).mfaLevel;
- var afkK = nico.guilds.cache.get(guild).afkChannel;
-var region = nico.guilds.cache.get(guild).region;
-var roller = nico.guilds.cache.get(guild).roles;
-var banner = nico.guilds.cache.get(guild).banner;
+ var mfalev = client.guilds.cache.get(guild).mfaLevel;
+ var afkK = client.guilds.cache.get(guild).afkChannel;
+var region = client.guilds.cache.get(guild).region;
+var roller = client.guilds.cache.get(guild).roles;
+var banner = client.guilds.cache.get(guild).banner;
+var embed = new Discord.MessageEmbed()
+.setThumbnail(`${banner || ""}`)
+.setColor("GREEN")
+.setAuthor()
+.setDescription(`
+${message.guild.name} Guild Info
 
+<><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+Verifaction Level : ${verfyseviye}
+Mfa Level : ${mfalev}
+Guild Region : ${region}
+Afk Channel : ${afkK || "Not Found"}
+
+<><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+BANNER URL : ${banner || "Not Added"}
+
+
+
+
+
+`);
+  message.channel.send(embed);
 };
 
 exports.conf = {

e52ae2a84ac43022ae248ea2db332458feb5c2c5
Glitch (hyper-gusty-node) <none>
2021-07-13 17:15:20 +0000

    🎃🏟 Checkpoint ./komutlar/help.js:31988211/2:33044891/71 ./server/web/html/admin/promo-new.ejs:31988211/3366

--- komutlar/help.js
+++ komutlar/help.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js')
 const disbut = require('discord-buttons') 
 const db = require("quick.db") 
-let fetchb = db.fetch(`bakim`)
 exports.run = async (client, message, args) => {
-  if(fetchb) return message.channel.send("**Maintance** Mode Has Enabled! ") 
-  else {
+ // if(fetchb) return message.channel.send("**Maintance** Mode Has Enabled! ") 
+  
   const embed_ = new Discord.MessageEmbed()
  .setImage("")
   /*.setDescription(`
@@ -52,7 +51,7 @@ exports.run = async (client, message, args) => {
 message.channel.send('', {embed: embed_, buttons: [davet, sp, dash, docs]});
 
 
-} 
+
   
 };
 exports.conf = {

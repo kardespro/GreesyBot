b08d188f27bbb4a54bdb1a462005d4ed145d09e0
Glitch (hyper-gusty-node) <none>
2021-07-14 07:26:43 +0000

    🗻👠 Checkpoint ./komutlar/daily.js:33044891/361

--- komutlar/daily.js
+++ komutlar/daily.js
@@ -1,8 +1,15 @@
 const Discord = require('discord.js') 
 const ms = require("parse-ms") 
+let db = require("quick.db") 
+                
 exports.run = async (client, message, args) => {
- let day = 86400000
- if(times !== null && (Date.now() - times) > 0) 
+ let times = db.fetch(`work_${message.author.id} `) 
+  let day = 86400000
+ 
+ if(times !== null && (Date.now() - times) > 0){
+   let time = ms(day - (Date.now() - times )) 
+   message.channel.send(`You Should Wait ${time.hours ? time.hours + "Hour": "" }  to Receive the Daily Reward! `) 
+  } 
    
  } 
 exports.conf = {

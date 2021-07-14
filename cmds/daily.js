7699947d9b0a4208e0b5d95714dbd6e4b4aeaea8
Glitch (hyper-gusty-node) <none>
2021-07-14 09:26:47 +0000

    🚅🚶 Checkpoint ./cmds/daily.js:31988211/14:33044891/466 ./server/web/html/index.ejs:31988211/114

--- cmds/daily.js
+++ cmds/daily.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
 const ms = require('parse-ms')
-exports.run = async(client, message) => {
-  const sakso = db.fetch(`premod_${message.guild.id}`)
+exports.run = async (client, message, args) => {
+ // const sakso = db.fetch(`premod_${message.guild.id}`)
   function rastgeleMiktar(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -15,25 +15,40 @@ let times = await db.fetch(`worktime_${message.author.id}`)
     message.channel.send(new Discord.MessageEmbed()
                         .setColor("RED")
                         .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-                        .setDescription(`⏱ Please Wait ${time.hours ? time.hours + " hour(s)": ""} ${time.minutes ? time.minutes + ' minute(s),' : ''} ${time.seconds ? time.seconds + ' second(s)!' : 'gir!'}`))
+                        .setDescription(`⏱ Please Wait ${time.hours ? time.hours + " hour(s)": ""} ${time.minutes ? time.minutes + ' minute(s),' : ''} ${time.seconds ? time.seconds + ' second(s)!' : '!'}`))
   return
   }
- var moneys = rastgeleMiktar(2000,3000)
+ var moneys = rastgeleMiktar(2000,2500)
+ var gcc = rastgeleMiktar(50,2000)
       message.channel.send(new Discord.MessageEmbed()
                    .setColor("YELLOW")
                    .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
                   // .setDescription(`Günlük, cüzdanına ${moneys} 💸 eklendi!`)
-                         .setDescription(``))
+                         .setDescription(`Your Daily Rewards Is **${moneys}** Coin And **${gcc}** GC! `))
 
 db.set(`worktime_${message.author.id}`, Date.now())
 
   db.add(`coin_${message.author.id}`, moneys)
+  db.add(`gc_${message.author.id}`, gcc)
+  console.log(`
+  Daily Reward! 
+  
+  Member ID ¦ ${message.author.id} 
+  Coin ¦ ${moneys} 
+  GC ¦ ${gcc} 
+  Gived | true
+  `)
  };
+//durrrrrrrrr
 exports.conf = {
   enabled: true,
+  guildOnly: false, 
   aliases: ["daily"],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'günlük',
+name: 'daily', 
+description: "", 
+usage: "" 
 };

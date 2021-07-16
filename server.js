bfdc19598682a83bfdbec7efba48835c85819483
Glitch (hyper-gusty-node) <none>
2021-07-16 19:25:12 +0000

    🤑💖 Checkpoint ./server.js:31988211/2

--- server.js
+++ server.js
@@ -642,7 +642,7 @@ app.get("/logout", (req, res) => {
 // SAYFALAR
 app.use("/t",require("./server/routers/index.js"));
 app.use("/blogt",require("./server/routers/blog.js"));
-app.use("*",require("./server/routers/404.js"));
+//app.use("*",require("./server/routers/404.js"));
 app.use("/dashtest",require("./server/routers/dashboard.js"));
 
 

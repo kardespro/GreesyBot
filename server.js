908070400dcede077e3654fb499f4b1e4a7832ed
Glitch (hyper-gusty-node) <none>
2021-07-29 08:48:55 +0000

    🐚🍉 Checkpoint ./server.js:31988211/4786

--- server.js
+++ server.js
@@ -2132,4 +2132,112 @@ nico.player.on("trackStart", (message, track) => {
 })
    } 
 });
+nico.on("guildMemberAdd", async member => {
+  const Discord = require("discord.js")
+const db = require("quick.db");
+const Canvas = require('canvas')
+    , Image = Canvas.Image
+    , Font = Canvas.Font
+    , path = require('path');
+const snekfetch = require('snekfetch');
+const request = require('node-superfetch');
+
+//module.exports = async member => {
+    var randomMsg = [
+                    "",      
+                    ];
+    var randomMsg_integer = randomMsg[Math.floor((Math.random() * randomMsg.length))]
+
+  let msj = await db.fetch(`girisM_${member.guild.id}`)
+  if (!msj) msj = `{uye}, ${randomMsg_integer}`
+  let memberChannel = await db.fetch(`gcc_${member.guild.id}`)
+  
+  const canvas = Canvas.createCanvas(360, 250);
+  const ctx = canvas.getContext('2d');
+  
+  const background = await Canvas.loadImage('https://media.discordapp.net/attachments/860622363175288842/870221346792144936/20210729_122834.jpg');
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  
+  ctx.strokeStyle = '#74037b';
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.fillStyle = `#D3D3D3`;
+  ctx.font = `25px "S&amp;S Nickson One"`;
+  ctx.textAlign = "center";
+  ctx.fillText(`${member.user.username.toUpperCase()}`, 170, 210);
+
+  let avatarURL = member.user.avatarURL() || member.user.defaultAvatarURL
+  const { body } = await request.get(avatarURL);
+  const avatar = await Canvas.loadImage(body);
+  
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.fill()
+  ctx.lineWidth = 4;
+ ctx.arc(112 + 55, 55 + 55, 55, 0, 2 * Math.PI, false);
+  ctx.clip();
+  ctx.drawImage(avatar, 112, 55, 110, 110);
+  
+  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'Logged-Greesy.jpg');
+  member.guild.channels.cache.get(memberChannel).send(attachment)
+  member.guild.channels.cache.get(memberChannel).send(msj.replace('{uye}', member).replace('{sunucu}', member.guild.name))
+  if (member.user.bot) return member.guild.channels.cache.get(memberChannel).send(`🤖 This Is Bot: ${member.user.tag}`)
+  
+
+  
+  });
+nico.on("guildMemberRemove", async member => {
+  //const Canvas = require('canvas') , Image = Canvas.Image , Font = Canvas.Font , path = require('path');const snekfetch = require('snekfetch');const request = require('node-superfetch');
+    
+const Discord = require("discord.js")
+  const Canvas = require('canvas')
+    , Image = Canvas.Image
+    , Font = Canvas.Font
+    , path = require('path');
+const snekfetch = require('snekfetch');
+const request = require('node-superfetch');
+  var randomMsg = [
+                    ""
+                    ];
+    var randomMsg_integer = randomMsg[Math.floor((Math.random() * randomMsg.length))]
+  
+  let paket = await db.fetch(`pakets_${member.id}`)
+  let memberChannel = await db.fetch(`gcc_${member.guild.id}`)
+  let msj = await db.fetch(`cikisM_${member.guild.id}`)
+  if (!msj) msj = `{uye}, ${randomMsg_integer}`
+  
+  
+  const canvas = Canvas.createCanvas(360, 250);
+ const ctx = canvas.getContext('2d');
+  
+  const background = await Canvas.loadImage('https://media.discordapp.net/attachments/860622363175288842/870221931411013644/20210729_123100.jpg');
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  
+  ctx.strokeStyle = '#74037b';
+ ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.fillStyle = `#D3D3D3`;
+ ctx.font = `25px "Warsaw"`;
+ ctx.textAlign = "center";
+ ctx.fillText(`${member.user.username.toUpperCase()}`, 170, 210);
+  
+  let avatarURL = member.user.avatarURL || member.user.defaultAvatarURL
+  const { body } = await request.get(avatarURL);
+ const avatar = await Canvas.loadImage(body);
+  
+  ctx.beginPath();
+ ctx.lineWidth = 4;
+  ctx.fill()
+ ctx.lineWidth = 4;
+ ctx.arc(112 + 55, 55 + 55, 55, 0, 2 * Math.PI, false);
+ ctx.clip();
+ ctx.drawImage(avatar, 112, 55, 110, 110);
+  
+  const attachment = new Discord.Attachment(canvas.toBuffer(), 'Left-Greesy.jpg');
+  member.guild.channels.get(memberChannel).send(attachment)
+  member.guild.channels.get(memberChannel).send(msj.replace('{uye}', member).replace('{sunucu}', member.guild.name));
+  if (member.user.bot) return member.guild.channels.get(memberChannel).send(`🤖 This Is Bot:, ${member.user.tag}`)
+  
+
+  });
 //});

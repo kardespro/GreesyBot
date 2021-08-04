9fd36860759ca06a7f9f921e8cbfcfdc8d66c57a
Glitch (hyper-gusty-node) <none>
2021-08-04 19:13:33 +0000

    🌜💺 Checkpoint ./server.js:33044891/11

--- server.js
+++ server.js
@@ -1556,7 +1556,7 @@ res.json(Nego);
 const keyRouter = require("./server/api/keyAuth.js");
 //app.use("/api/",keyRouter)
 
-nico.login(process.env.token2);
+nico.login(process.env.TOKEN);
 const listener = app.listen(process.env.PORT, () => {
   /*console.log("Your app is listening on port " + listener.address().port);
 

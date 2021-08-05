d5e0dff7482ba6419893413dba7cf6ba3c2cd132
Glitch (hyper-gusty-node) <none>
2021-08-05 14:28:51 +0000

    🚌🏈 Checkpoint ./commands/blacklist.js:33044891/917

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,15 +1,31 @@
 const Discord = require('discord.js');
-const dbb = require("../Database/main.js") 
-const db = new dbb("./all.js") 
+//const dbb = require("../Database/main.js") 
+const db = require("quick.db") 
 //const axios = require('axios');
 
 exports.run = async(client, message, args) => {
+  var ownirs = ["852853360612605952","682607343707488388"] 
+  if(!message.author.id === ownirs.includes) return;
+  if(!args[0]) return message.channel.send("<:hayirbei:867465654960128010> || \`add, remove\`") 
   if(args[0] == "add"){
 const kk = message.mentions.users.first() || args[1];
   if(!kk) return message.channel.send("<:hayirbei:867465654960128010> | Please Mention a **User**! ") 
 db.set(`blacklist.${kk.id}`, true) 
-message.channel.send(`${kk.tag}${kk.discriminator`) 
+message.channel.send(`<:evetbei:867465536736460810> | \`${kk.tag}#${kk.discriminator}\`(${kk.id}) Added To BlackList!`) 
+    client.channels.cache.get("").send(`
+     **API Log**
+    > User: \`${kk.tag}\`
+    > Discriminator: \`${kk.discriminator}\`
+    > Moderator: ${message.author.id} 
+    `)
   } 
+  if(args[0] == "remove"){
+    const kk2 = message.mentions.users.first() || args[1];
+    if(!kk2) return message.channel.send("<:hayirbei:867465654960128010> | Please Mention a **User**!") 
+    db.delete(`blacklist.${kk2.id}`)
+    return message.channel.send("<:evetbei:867465536736460810> | Removed! ") 
+    
+   } 
 };
 
 exports.conf = { aliases: [] };
